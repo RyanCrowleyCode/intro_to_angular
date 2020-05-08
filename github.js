1f7dd63077a40755ea1d20b483ce1f12cd96466d
Ryan Crowley <RyanCrowley@Ryan-Crowley-MacBook.local>
2020-05-08 16:20:31 -0500

    Completed the course.

--- github.js
+++ github.js
@@ -1,31 +1,30 @@
-(function(){
-
-    var github = function($http){
-  
-      var getUser = function(username){
-        return $http.get('https://api.github.com/users/' + username)
-          .then(function(response){
+(function() {
+    var github = function($http) {
+      var getUser = function(username) {
+        return $http
+          .get('https://api.github.com/users/' + username)
+          .then(function(response) {
             return response.data;
-          })
+          });
       };
   
-      var getRepos = function(user){
-        return $http.get(user.repos_url)
-          .then(function(response){
+      var getRepos = function(user) {
+        return $http
+          .get(user.repos_url + '?per_page=100')
+          .then(function(response) {
             return response.data;
           });
       };
   
       return {
         getUser: getUser,
-        getRepos: getRepos
+        getRepos: getRepos,
       };
-  
     };
   
-    var module = angular.module("githubViewer");
+    var module = angular.module('githubViewer');
   
     // Register the SERVICE with Angular
-    module.factory("github", github);
-  
+    module.factory('github', github);
   })();
+  
